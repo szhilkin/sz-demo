b5b9987bd28c0fe2f5e4ccf4cf22ab8e8d1e914f
Sergey Zhilkin <szhilkin@gmail.com>
2017-05-26 17:28:13 +0300

    moved line inside

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 require 'prome/web'
-mount Prome::Web, at: "/metrics"
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Prome::Web, at: "/metrics" 
 end
